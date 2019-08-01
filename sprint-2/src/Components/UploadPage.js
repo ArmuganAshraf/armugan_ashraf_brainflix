17f1e589d80e11b8df71e2e0710dfc40f33cfea6
Armugan Ashraf <armugana1@gmail.com>
2019-08-01 06:40:45 -0400

    styled the upload page for mobile site

--- sprint-2/src/Components/UploadPage.js
+++ sprint-2/src/Components/UploadPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from './NavBar';
+import '../App.scss';
 import Thumbnail from '../Images/Upload-video-preview.jpg';
 
 class UploadPage extends React.Component{
@@ -17,16 +18,23 @@ class NewPhoto extends React.Component{
   render(){
     return(
       <>
-        <div>
+        <div className='upload'>
           <h1>Upload Video</h1>
-          <label>VIDEO THUMBNAIL</label>
-          <img src={Thumbnail} alt='upload photo'/>
-          <label>TITLE YOUR VIDEO</label>
-          <input type='text' placeholder='Add a title to your video' />
-          <label>ADD A VIDEO DESCRIPTION</label>
-          <input type='text' placeholder='Add a description of your video'/>
-          <button>PUBLISH</button>
-          <button>CANCEL</button>
+          <div>
+            <label>VIDEO THUMBNAIL</label>
+            <img src={Thumbnail} alt='upload photo'/>
+          </div>
+          <div>
+            <label>TITLE YOUR VIDEO</label>
+            <input type='text' placeholder='Add a title to your video' />
+            <label>ADD A VIDEO DESCRIPTION</label>
+            <input type='text' placeholder='Add a description of your video'/>
+          </div>
+          <div>
+            <button className='upload__button1'>PUBLISH</button>
+            <button className='upload__button2'>CANCEL</button>
+          </div>
+          
         </div>
       </>
     )
