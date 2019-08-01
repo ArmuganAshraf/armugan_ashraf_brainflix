97592d981d246d22f46a9abe8edfa11824861c00
Armugan Ashraf <armugana1@gmail.com>
2019-08-01 06:14:26 -0400

    added content to the upload page

--- sprint-2/src/Components/UploadPage.js
+++ sprint-2/src/Components/UploadPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import NavBar from './NavBar';
-import Thumbnail from '../Images/'
+import Thumbnail from '../Images/Upload-video-preview.jpg';
 
 class UploadPage extends React.Component{
   render(){
     return(
       <>
         <NavBar />
+        <NewPhoto />
       </>
     )
   }
@@ -19,7 +20,13 @@ class NewPhoto extends React.Component{
         <div>
           <h1>Upload Video</h1>
           <label>VIDEO THUMBNAIL</label>
-          <img src={Thumbnail} />
+          <img src={Thumbnail} alt='upload photo'/>
+          <label>TITLE YOUR VIDEO</label>
+          <input type='text' placeholder='Add a title to your video' />
+          <label>ADD A VIDEO DESCRIPTION</label>
+          <input type='text' placeholder='Add a description of your video'/>
+          <button>PUBLISH</button>
+          <button>CANCEL</button>
         </div>
       </>
     )
